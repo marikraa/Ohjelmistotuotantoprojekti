ccef2c6a2dddf7f2913553aa4472786e661961b1
Ville <villesc@gmail.com>
2025-01-30 14:48:00 +0200

    mvc kansio

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -1,20 +1,35 @@
 package View;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.stage.Stage;
+
+import java.awt.*;
 import java.io.IOException;
 
 
 public class View extends Application {
+
+
+
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(View.class.getResource("UI.fxml"));
-        // JavaFX-sovelluksen (käyttöliittymän) käynnistäminen
-        Scene scene = new Scene(loader.load(), 320, 240);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(View.class.getResource("UI.fxml"));
+        fxmlLoader.setController(this);
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("Note wizard");
+
+        primaryStage.setOnCloseRequest(evt -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+
 
     }
 
