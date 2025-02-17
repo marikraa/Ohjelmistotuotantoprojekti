09acb41c6a2d6e9e0c165eacd3a7ec3c17e76c62
Ville <villesc@gmail.com>
2025-02-17 17:31:44 +0200

    note merge from Liu

--- src/main/java/View/NoteAddController.java
+++ src/main/java/View/NoteAddController.java
@@ -3,16 +3,19 @@ package View;
 import Controller.Controller;
 import Model.Note;
 import Model.User;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class NoteAddController {
+    public DatePicker dateSelector;
+    public Spinner hoursSpinner;
+    public Spinner minutesSpinner;
     ImageAdder imageAdder;
     public TextField titleField;
     IControllerForGUI controller;
@@ -20,14 +23,24 @@ public class NoteAddController {
     public ImageView noteImage;
     public TextArea noteContent;
     private Stage noteStage;
+    DatePicker datePicker;
     Image selectedImage;
     User user;
     public NoteAddController() {
         imageAdder = new ImageAdder();
         controller = Controller.getInstance();
         user = SessionManager.getCurrentUser();
+       datePicker = new DatePicker();
+    }
+    public void initialize(){
+        datePicker.setValue(LocalDate.now());
+        hoursSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23, 0));
+        minutesSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 0));
     }
 
+
+
+    //this is method is called when the user clicks the add note button
     public void addNote(MouseEvent mouseEvent) {
         List<Note> notes =  controller.addNote(user.getUsername(),titleField.getText(),noteContent.getText(),selectedImage,dueDateField.getText());
         if(notes==null){
@@ -42,12 +55,16 @@ public class NoteAddController {
 
 
     }
-
+    //this method is called when the wants to add an image to the note
     public void addNoteImage(MouseEvent mouseEvent) {
        selectedImage = imageAdder.addPicture(mouseEvent);
+        if (selectedImage != null) {
+            noteImage.setImage(selectedImage);
+        }
+
 
     }
-
+    //this is setter method for the stage so stage can be closed when the note is added
     public void setStage(Stage addNoteStage) {
         this.noteStage= addNoteStage;
 
