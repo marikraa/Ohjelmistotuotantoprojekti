e63f14026c2c990210ba93ea0b76eeccfc934c6e
Ville <villesc@gmail.com>
2025-03-31 15:22:22 +0300

    main screen locale made

--- src/main/java/View/controllers/MainScreenController.java
+++ src/main/java/View/controllers/MainScreenController.java
@@ -27,11 +27,18 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 
 public class MainScreenController implements UiInterface {
+
+    public Button logoutButton;
+    Locale locale = SessionManager.getLocale();
+    ResourceBundle rb = ResourceBundle.getBundle("language", locale);
     private IControllerForGUI controller;
     public TextField searchField;
     public ImageView profilePic;
@@ -53,10 +60,14 @@ public class MainScreenController implements UiInterface {
     }
 
     public void initialize() {
+
+        searchField.setPromptText(rb.getString("searchNote"));
+        logoutButton.setText(rb.getString("logout"));
         int noteCount = user.getNotes().size();
         String username = user.getUsername();
         usernameLabel.setText(username);
-        noteCounterLabel.setText("Notes: " + noteCount);
+        String noteLabel=  MessageFormat.format(rb.getString("notes"), noteCount);
+        noteCounterLabel.setText(noteLabel);
         profilePic.setImage(new Image(user.getProfilePictureUrl()));
         System.out.println("Main init");
         drawNotes();
